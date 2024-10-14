b27fe3d183f68420c8eb74f870e915d0edea5e91
Jacqueline Taylor <jacquelinetaylor644@gmail.com>
2024-10-13 21:47:54 -0700

    Solve multiples problem using for loop, filter method, and reduce method

---
+++ 6kyu/multiples-of-3-or-5.js
@@ -0,0 +1,32 @@
+// Description:
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Additionally, if the number is negative, return 0.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number){
+
+    if (number<=3) {
+      return 0
+    }
+  
+    else {
+      
+      let arrOfNums = []
+      for (i=1;i<number;i++) {
+        arrOfNums.push(i)
+      }
+    
+      let filteredNums = arrOfNums.filter(
+        (num) => (num % 3 == 0) || (num % 5 ==0)
+      )
+      
+      let sum = filteredNums.reduce((acc, cur) => acc + cur)
+      
+      return sum
+    }
+  
+  }
