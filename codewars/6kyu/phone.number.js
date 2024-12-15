3a261181ff4276a1c345b9868300b918f754980e
Jacqueline Taylor <jacquelinetaylor644@gmail.com>
2024-12-14 19:04:52 -0800

    Refactor solution for readability

--- codewars/6kyu/phone.number.js
+++ codewars/6kyu/phone.number.js
@@ -3,3 +3,14 @@
 function createPhoneNumber(numbers){
     return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`
   }
+
+// More readable solution
+
+function createPhoneNumber(numbers){
+  
+    const areaCode = numbers.slice(0,3).join('')
+    const first3 = numbers.slice(3,6).join('')
+    const last4 = numbers.slice(6).join('')
+    
+    return `(${areaCode}) ${first3}-${last4}`
+  }
