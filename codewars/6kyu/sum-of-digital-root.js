121a1df9305fcf1618c2001e355c1bfd2afb0646
Jacqueline Taylor <jacquelinetaylor644@gmail.com>
2024-12-16 12:52:15 -0800

    Solve sum of digital root

---
+++ codewars/6kyu/sum-of-digital-root.js
@@ -0,0 +1,21 @@
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digitalRoot(n) {
+
+    let arrOfDigits = n.toString().split('').map(digit => Number(digit))
+    let sum = arrOfDigits.reduce((acc,cur) => acc + cur)
+    
+    if (sum.toString().length > 1) {
+      sum = digitalRoot(sum)
+    }
+    
+    return sum
+  }
