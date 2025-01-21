ee9974d2cbd8c449bd9c14bc6a674e614f2dacef
Jacqueline Taylor <jacquelinetaylor644@gmail.com>
2025-01-21 13:15:00 -0700

    Solve 2725

---
+++ leetcode/easy/2725.js
@@ -0,0 +1,26 @@
+// 2725. Interval Cancellation
+// Easy
+// Companies
+// Given a function fn, an array of arguments args, and an interval time t, return a cancel function cancelFn.
+
+// After a delay of cancelTimeMs, the returned cancel function cancelFn will be invoked.
+
+// setTimeout(cancelFn, cancelTimeMs)
+// The function fn should be called with args immediately and then called again every t milliseconds until cancelFn is called at cancelTimeMs ms.
+
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    fn(...args)
+
+    const timerID = setInterval(() => {fn(...args)}, t)
+
+    return function cancelFn (){
+        clearInterval(timerID)
+    }
+    
+};
